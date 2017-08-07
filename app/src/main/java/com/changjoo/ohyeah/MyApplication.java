c3f927323a9760d4a1d93dda4f8149c73b83e800
Changjoo <thu512@naver.com>
2017-08-07 09:57:45 +0900

    메인,회원가입 ,65k 수정

---
+++ app/src/main/java/com/changjoo/ohyeah/MyApplication.java
@@ -0,0 +1,16 @@
+package com.changjoo.ohyeah;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Tacademy on 2017-08-07.
+ */
+
+public class MyApplication extends MultiDexApplication
+{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
